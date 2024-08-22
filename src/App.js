edb9242c2c31ed4e15c5ec23475a9d971c0e5a08
Akash Sathe <akash.sathe@myn.global>
2024-08-22 10:33:40 +0530

    implementes a new path r pages by using react-router-dom liabrary.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header.js";
 import Body from "./components/Body.js";
-
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./components/About.js";
+import Contact from "./components/Contact.js";
+import Error from "./components/Error.js";
 // const heading = React.createElement("h1",{},"namaste react");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
@@ -32,6 +35,21 @@ const Applayout = () =>{
       <Body/>
     </div>)
 }
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element:<Applayout/>,
+    errorElement:<Error/>
+  },
+  {
+    path:"/about",
+    element: <About/>
+  },
+  {
+    path:"/contact",
+    element: <Contact/>
+  }
+])
  const root = ReactDOM.createRoot(document.getElementById("root")); 
- root.render(<Applayout/>);
- 
+//  root.render(<Applayout/>);
+ root.render(<RouterProvider router={appRouter}/>)
